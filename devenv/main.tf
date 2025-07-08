c32e5c4bd6c1c1ab5d8d3f4be458f67ec2a92df8
muqeeth <muqeeth23@gmail.com>
2025-07-08 14:41:21 +0530

    added s3 backend

--- devenv/main.tf
+++ devenv/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "my-bucket-1099"
+    key    = "dev.tfstate"
+    region = "us-east-1"
+  }
+}
+
 module "dev" {
   source = "../modules/vpc_module"
   vpc_cidr = "10.0.0.0/16"
